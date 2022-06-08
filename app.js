6e9f96b2f05343d4b13bba8eb91b6e3783efce66
Axel Iota <iota.axel@gmail.com>
2022-06-08 11:04:44 -0400

    Update start, comment

--- app.js
+++ app.js
@@ -82,6 +82,6 @@ app.get("/contact", function(req, res){
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server has started successfully.");
 });
 
