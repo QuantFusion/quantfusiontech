ec8641df5b61aabeabab9659e30ce523fd288710
Axel Iota <iota.axel@gmail.com>
2022-06-08 11:52:35 -0400

    Add blogdb env

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.static("public"));
 
 const blog_user = process.env.BUSINESS_BLOG_MONGO_ATLAS_USER;
 const blog_pass = encodeURIComponent(process.env.BUSINESS_BLOG_MONGO_ATLAS_PASS);
-mongoose.connect(`mongodb+srv://${blog_user}:${blog_pass}@cluster0.0v7ihz6.mongodb.net/blogDB`, {useNewUrlParser: true});
+const blog_db = encodeURIComponent(process.env.BUSINESS_BLOG_MONGO_ATLAS_DB);
+mongoose.connect(`mongodb+srv://${blog_user}:${blog_pass}@cluster0.0v7ihz6.mongodb.net/${blog_db}?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
