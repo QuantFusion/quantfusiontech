bb8e3b283984379d1608beea885de81ec086c41a
Axel Iota <iota.axel@gmail.com>
2022-06-08 12:34:54 -0400

    Remove encoding

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 const blog_user = process.env.BUSINESS_BLOG_MONGO_ATLAS_USER;
-const blog_pass = encodeURIComponent(process.env.BUSINESS_BLOG_MONGO_ATLAS_PASS);
+const blog_pass = process.env.BUSINESS_BLOG_MONGO_ATLAS_PASS;
 const blog_db = process.env.BUSINESS_BLOG_MONGO_ATLAS_DB;
 mongoose.connect(`mongodb+srv://${blog_user}:${blog_pass}@cluster0.0v7ihz6.mongodb.net/${blog_db}?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
